ca8d4df4c99acdca29a9e1e9da0e21300432a521
Marina Rivas <jcairoli1993@gmail.com>
2021-12-12 10:48:34 -0600

    react course

--- src/App.js
+++ src/App.js
@@ -11,12 +11,21 @@ class App extends Component{
     this.state = {
       monsters: [],
       searchField: ''
-  }}
+
+  };
+  
+}
+
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters: users}))}
+
+
+    handleChange = (e)  => {
+      this.setState({searchField: e.target.value})
+    }
 
   render (){
     const { monsters, searchField} = this.state;
@@ -25,10 +34,11 @@ class App extends Component{
       );
     return (
       <div className="App">
+      <h1>Monster Rolodex</h1>
         
         <SearchBox
           placeholder='Search Monster' 
-          handleChange={e => this.setState({searchField: e.target.value})} 
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}>
 
